75c7a3dd13b7c7b91dcbd738bba060fd3e71a426
ShawyYum <111735776+ShawyYum@users.noreply.github.com>
2023-10-11 01:55:58 +0800

    Lexer.java

--- lexer/Lexer.java
+++ lexer/Lexer.java
@@ -4,196 +4,194 @@ import token.Token;
 import java.util.Objects;
 
 public class Lexer {
-    public static class lexer {
-        private final String input;
-        private int curPosition;
-        private int nextPosition;
-        private char ch;
+    private final String input;
+    private int curPosition;
+    private int nextPosition;
+    private char ch;
 
-        public lexer(String input) {
-            this.input = input;
+    public Lexer(String input) {
+        this.input = input;
+        readNext();
+    }
+
+    public Token NextToken() {
+        Token tok;
+
+        skipWhitespace();
+
+        switch(ch) {
+            case '=':
+                if(Objects.equals(peekChar(),'=')) {
+                    readNext();
+                    tok = new Token(Token.EQ,"==");
+                }
+                else {
+                    tok = newToken(Token.ASSIGN,ch);
+                }
+                break;
+            case '+':
+                tok = newToken(Token.PLUS,ch);
+                break;
+            case '-':
+                tok = newToken(Token.MINUS,ch);
+                break;
+            case '!':
+                if(Objects.equals(peekChar(),'=')) {
+                    readNext();
+                    tok = new Token(Token.NOT_EQ,"!=");
+                }
+                else {
+                    tok = newToken(Token.BANG,ch);
+                }
+                break;
+            case '/':
+                tok = newToken(Token.SLASH,ch);
+                break;
+            case '*':
+                tok = newToken(Token.ASTERISK,ch);
+                break;
+            case '<':
+                if(Objects.equals(peekChar(),'=')) {
+                    readNext();
+                    tok = new Token(Token.LTASSIGN,"<=");
+                }
+                else {
+                    tok = newToken(Token.LT,ch);
+                }
+                break;
+            case '>':
+                if(Objects.equals(peekChar(),'=')) {
+                    readNext();
+                    tok = new Token(Token.GTASSIGN,">=");
+                }
+                else {
+                    tok = newToken(Token.GT,ch);
+                }
+                break;
+            case '{':
+                tok = newToken(Token.LBRACE,ch);
+                break;
+            case '}':
+                tok = newToken(Token.RBRACE,ch);
+                break;
+            case '(':
+                tok = newToken(Token.LPAREN,ch);
+                break;
+            case ')':
+                tok = newToken(Token.RPAREN,ch);
+                break;
+            case '\'':
+                tok = new Token(Token.CHAR,readChar());
+                break;
+            case '"':
+                tok = new Token(Token.STRING,readString());
+                break;
+            case '[':
+                tok = newToken(Token.LBRACKET,ch);
+                break;
+            case ']':
+                tok = newToken(Token.RBRACKET,ch);
+                break;
+            case ';':
+                tok = newToken(Token.SEMICOLON,ch);
+                break;
+            case ':':
+                tok = newToken(Token.COLON,ch);
+                break;
+            case ',':
+                tok = newToken(Token.COMMA,ch);
+                break;
+            case 0:
+                tok = new Token(Token.EOF,"");
+                break;
+            default:
+                if(Character.isLetter(ch) || Objects.equals(ch,'_')) {
+                    String tmp = readIdentifier();
+                    tok = new Token(Token.LookupIdent(tmp),tmp);
+                    return tok;
+                }
+                else if(Character.isDigit(ch)) {
+                    tok = new Token(Token.INT,readNumber());
+                    return tok;
+                }
+                else {
+                    tok = newToken(Token.ILLEAGL,ch);
+                }
+                break;
+        }
+
+        readNext();
+        return tok;
+    }
+
+    private void skipWhitespace() {
+        while(Objects.equals(ch,' ') || Objects.equals(ch,'\t') || Objects.equals(ch,'\n') || Objects.equals(ch,'\r')) {
+            readNext();
+        }
+    }
+
+    private void readNext() {
+        if(nextPosition >= input.length()) {
+            ch = 0;
+        }
+        else {
+            ch = input.charAt(nextPosition);
+        }
+        curPosition = nextPosition;
+        nextPosition += 1;
+    }
+
+    private char peekChar() {
+        if(nextPosition >= input.length()) {
+            return 0;
+        }
+        else {
+            return input.charAt(nextPosition);
+        }
+    }
+
+    private String readIdentifier() {
+        int position = curPosition;
+
+        while (Character.isLetter(ch) || Objects.equals(ch,'_')) {
             readNext();
         }
 
-        public Token.token NextToken() {
-            Token.token tok;
+        return input.substring(position, curPosition);
+    }
 
-            skipWhitespace();
+    private String readNumber() {
+        int position = curPosition;
 
-            switch(ch) {
-                case '=':
-                    if(Objects.equals(peekChar(),'=')) {
-                        readNext();
-                        tok = new Token.token(Token.EQ,"==");
-                    }
-                    else {
-                        tok = newToken(Token.ASSIGN,ch);
-                    }
-                    break;
-                case '+':
-                    tok = newToken(Token.PLUS,ch);
-                    break;
-                case '-':
-                    tok = newToken(Token.MINUS,ch);
-                    break;
-                case '!':
-                    if(Objects.equals(peekChar(),'=')) {
-                        readNext();
-                        tok = new Token.token(Token.NOT_EQ,"!=");
-                    }
-                    else {
-                        tok = newToken(Token.BANG,ch);
-                    }
-                    break;
-                case '/':
-                    tok = newToken(Token.SLASH,ch);
-                    break;
-                case '*':
-                    tok = newToken(Token.ASTERISK,ch);
-                    break;
-                case '<':
-                    if(Objects.equals(peekChar(),'=')) {
-                        readNext();
-                        tok = new Token.token(Token.LTASSIGN,"<=");
-                    }
-                    else {
-                        tok = newToken(Token.LT,ch);
-                    }
-                    break;
-                case '>':
-                    if(Objects.equals(peekChar(),'=')) {
-                        readNext();
-                        tok = new Token.token(Token.GTASSIGN,">=");
-                    }
-                    else {
-                        tok = newToken(Token.GT,ch);
-                    }
-                    break;
-                case '{':
-                    tok = newToken(Token.LBRACE,ch);
-                    break;
-                case '}':
-                    tok = newToken(Token.RBRACE,ch);
-                    break;
-                case '(':
-                    tok = newToken(Token.LPAREN,ch);
-                    break;
-                case ')':
-                    tok = newToken(Token.RPAREN,ch);
-                    break;
-                case '\'':
-                    tok = new Token.token(Token.CHAR,readChar());
-                    break;
-                case '"':
-                    tok = new Token.token(Token.STRING,readString());
-                    break;
-                case '[':
-                    tok = newToken(Token.LBRACKET,ch);
-                    break;
-                case ']':
-                    tok = newToken(Token.RBRACKET,ch);
-                    break;
-                case ';':
-                    tok = newToken(Token.SEMICOLON,ch);
-                    break;
-                case ':':
-                    tok = newToken(Token.COLON,ch);
-                    break;
-                case ',':
-                    tok = newToken(Token.COMMA,ch);
-                    break;
-                case 0:
-                    tok = new Token.token(Token.EOF,"");
-                    break;
-                default:
-                    if(Character.isLetter(ch) || Objects.equals(ch,'_')) {
-                        String tmp = readIdentifier();
-                        tok = new Token.token(Token.LookupIdent(tmp),tmp);
-                        return tok;
-                    }
-                    else if(Character.isDigit(ch)) {
-                        tok = new Token.token(Token.INT,readNumber());
-                        return tok;
-                    }
-                    else {
-                        tok = newToken(Token.ILLEAGL,ch);
-                    }
-                    break;
-            }
-
+        while (Character.isDigit(ch)) {
             readNext();
-            return tok;
         }
 
-        private void skipWhitespace() {
-            while(Objects.equals(ch,' ') || Objects.equals(ch,'\t') || Objects.equals(ch,'\n') || Objects.equals(ch,'\r')) {
-                readNext();
-            }
-        }
+        return input.substring(position, curPosition);
+    }
 
-        private void readNext() {
-            if(nextPosition >= input.length()) {
-                ch = 0;
-            }
-            else {
-                ch = input.charAt(nextPosition);
-            }
-            curPosition = nextPosition;
-            nextPosition += 1;
-        }
+    private String readChar() {
+        int position = curPosition + 1;
 
-        private char peekChar() {
-            if(nextPosition >= input.length()) {
-                return 0;
-            }
-            else {
-                return input.charAt(nextPosition);
-            }
-        }
+        do {
+            readNext();
+        } while(!Objects.equals(ch,'\'') && !Objects.equals(ch,0));
 
-        private String readIdentifier() {
-            int position = curPosition;
+        return input.substring(position,curPosition);
+    }
 
-            while (Character.isLetter(ch) || Objects.equals(ch,'_')) {
-                readNext();
-            }
+    private String readString() {
+        int position = curPosition + 1;
 
-            return input.substring(position, curPosition);
-        }
+        do {
+            readNext();
+        } while (!Objects.equals(ch,'"') && !Objects.equals(ch,0));
 
-        private String readNumber() {
-            int position = curPosition;
+        return input.substring(position, curPosition);
+    }
 
-            while (Character.isDigit(ch)) {
-                readNext();
-            }
-
-            return input.substring(position, curPosition);
-        }
-
-        private String readChar() {
-            int position = curPosition + 1;
-
-            do {
-                readNext();
-            } while(!Objects.equals(ch,'\'') && !Objects.equals(ch,0));
-
-            return input.substring(position,curPosition);
-        }
-
-        private String readString() {
-            int position = curPosition + 1;
-
-            do {
-                readNext();
-            } while (!Objects.equals(ch,'"') && !Objects.equals(ch,0));
-
-            return input.substring(position, curPosition);
-        }
-
-        private Token.token newToken(String tokenType,char ch) {
-            return new Token.token(tokenType,String.valueOf(ch));
-        }
+    private Token newToken(String tokenType,char ch) {
+        return new Token(tokenType,String.valueOf(ch));
     }
 }
 
