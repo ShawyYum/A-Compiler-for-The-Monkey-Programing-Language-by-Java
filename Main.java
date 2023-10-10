ee19f9170d0ec6eda088c3f3d946ef7e60359d31
ShawyYum <111735776+ShawyYum@users.noreply.github.com>
2023-10-11 02:45:17 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import repl.Repl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!This is the Chi programing language!");
+        System.out.println("Hello!This is the Monkey programing language!");
         System.out.println("Feel free to type in commands");
         System.out.println("exit with ctrl D or ctrl Z");
 
